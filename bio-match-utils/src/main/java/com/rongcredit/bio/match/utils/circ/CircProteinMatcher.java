e2f133b8b3b18f84ff270b0581b92f5efed314b5
oujian <oujian@rongcredit.com>
2025-04-03 20:52:41 +0800

    fix

--- bio-match-utils/src/main/java/com/rongcredit/bio/match/utils/circ/CircProteinMatcher.java
+++ bio-match-utils/src/main/java/com/rongcredit/bio/match/utils/circ/CircProteinMatcher.java
@@ -15,139 +15,155 @@ import com.rongcredit.bio.match.utils.TranslateResult;
 
 public class CircProteinMatcher implements ProteinMatcher {
 
-    private Map<String, CircProtein> sequence2ProteinMapCache = new HashMap<>();
-    private final ProteinTranslator  proteinTranslator        = new ProteinTranslator();
-    private ReentrantLock            cacheLock                = new ReentrantLock();
-    private int                      circLoop                 = 4;
+	private Map<String, CircProtein> sequence2ProteinMapCache = new HashMap<>();
+	private final ProteinTranslator proteinTranslator = new ProteinTranslator();
+	private ReentrantLock cacheLock = new ReentrantLock();
+	private final int circLoop;
+	private final boolean checkBoundary;
 
-    private CircProtein toCirc(final String sequence, int loop) {
-        StringBuilder builder = new StringBuilder();
-        String remainSeq = null;
-        List<Integer> boundarys = new ArrayList<>();
-        int boundary = 0;
-        for (int i = 0; i < loop; i++) {
-            String subSeq;
-            int length = sequence.length();
-            if (remainSeq != null && !remainSeq.isBlank() && remainSeq.length() < 3) {
-                subSeq = remainSeq + sequence;
-                length -= (3 - remainSeq.length());
-            } else {
-                subSeq = sequence;
-            }
+	public CircProteinMatcher() {
+		this.circLoop = 4;
+		this.checkBoundary = true;
+	}
 
-            TranslateResult translateResult = proteinTranslator.translate(subSeq);
-            String protein = translateResult.getProtein();
-            builder.append(protein);
-            boundary += length / 3;
+	public CircProteinMatcher(int circLoop, boolean checkBoundary) {
+		this.circLoop = circLoop;
+		this.checkBoundary = checkBoundary;
+	}
 
-            remainSeq = translateResult.getRemainSequence();
-            if (remainSeq != null && !remainSeq.isBlank()) {
-                boundary += 1;
-            }
-            // ignore the last boundary
-            // append boundary
-            if ((i + 1) < loop) {
-                boundarys.add(boundary);
-            }
-        }
-        return new CircProtein(builder.toString(), boundarys);
-    }
+	private CircProtein toCirc(final String sequence, int loop) {
+		StringBuilder builder = new StringBuilder();
+		String remainSeq = null;
+		List<Integer> boundarys = new ArrayList<>();
+		int boundary = 0;
+		for (int i = 0; i < loop; i++) {
+			String subSeq;
+			int length = sequence.length();
+			if (remainSeq != null && !remainSeq.isBlank() && remainSeq.length() < 3) {
+				subSeq = remainSeq + sequence;
+				length -= (3 - remainSeq.length());
+			} else {
+				subSeq = sequence;
+			}
 
-    private CircProtein translate(final String sequence) {
-        if (sequence == null) {
-            throw new IllegalArgumentException("The DNA/RNA sequence can not be null");
-        }
-        String normalizedSequence = sequence.toUpperCase();
-        CircProtein protein = sequence2ProteinMapCache.get(normalizedSequence);
-        if (protein != null) {
-            return protein;
-        }
-        cacheLock.lock();
-        try {
-            if (protein == null) {
-                protein = toCirc(normalizedSequence, circLoop);
-                sequence2ProteinMapCache.put(sequence, protein);
-            }
-            return protein;
-        } finally {
-            cacheLock.unlock();
-        }
-    }
+			TranslateResult translateResult = proteinTranslator.translate(subSeq);
+			String protein = translateResult.getProtein();
+			builder.append(protein);
+			boundary += length / 3;
 
-    public static void main(String[] args) {
-        CircProteinMatcher matcher = new CircProteinMatcher();
-        final String rna = "ATTGCAGGAGGAGATGCTTCAGAGAGAGGAAGCCGAAAACACCCTGCAATCTTTCAGACAGGATGTGATTACAAGGATGACGACGATAAGTGACAATGCGTCTCTGGCACGTCTTGACCTTGAACGCAAAGTGGAATCTTTGCAAGAAGAGATTGCCTTTTTGAAGAAACTCCACGAAGAGGAAATCCAGGAGCTGCAGGCTCAGATTCAGGAACAGCATGTCCAAATCGATGTGGATGTTTCCAAGCCTGACCTCACGGCTGCCCTGCGTGACGTACGTCAGCAATATGAAAGTGTGGCTGCCAAGAACCTGCAGGAGGCAGAAGAATGGTACAAATCCAAGTTTGCTGACCTCTCTGAGGCTGCCAACCGGAACAATGACGCCCTGCGCCAGGCAAAGCAGGAGTCCACTGAGTACCGGAGACAGGTGCAGTCCCTCACCTGTGAAGTGGATGCCCTTAAAGGAACCAATGAGTCCCTGGAACGCCAGATGCGTGAAATGGAAGAGAACTTTGCCGTTGAAGCTGCTAACTACCAAGACACTATTGGCCGCCTGCAGGATGAGATTCAGAATATGAAGGAGGAAATGGCTCGTCACCTTCGTGAATACCAAGACCTGCTCAATGTTAAGATGGCCCTTGACATTGAGATTGCCACCTACAGGAAGCTGCTGGAAGGCGAGGAGAGCAGGATTTCTCTGCCTCTTCCAAACTTTTCCTCCCTGAACCTGAGGGAAACTAATCTGGATTCACTCCCTCTGGTTGATACCCACTCAAAAAGGACACTTCTGATTAAGACGGTTGAAACTAGAGATGGACAG";
-        CircProtein protein = matcher.translate(rna);
-        System.out.println(protein.getProtein());
-        System.out.println(Arrays.toString(protein.getBoundarys().toArray()));
-        matcher.match(null, rna, "VETRDGQIAGGDASER");
-    }
+			remainSeq = translateResult.getRemainSequence();
+			if (remainSeq != null && !remainSeq.isBlank()) {
+				boundary += 1;
+			}
+			// ignore the last boundary
+			// append boundary
+			if ((i + 1) < loop) {
+				boundarys.add(boundary);
+			}
+		}
+		return new CircProtein(builder.toString(), boundarys);
+	}
 
-    private MatchResult match(final String RNAKey, final String RNA, final String protein) {
-        // cache the RNA
-        CircProtein circProtein = translate(RNA);
-        String target = circProtein.getProtein();
-        List<Integer> boundarys = circProtein.getBoundarys();
-        // do match
-        final int pl = protein.length();
-        Integer matchedBoundary = null;
-        Integer matchedIndex = null;
-        for (int fromIndex = 0; fromIndex < target.length(); fromIndex++) {
-            int index = target.indexOf(protein, fromIndex);
-            if (index == -1) {
-                break;
-            }
-            fromIndex = index + 1;
-            // check boundary
-            for (Integer boundary : boundarys) {
-                if ((index <= boundary - 2) && ((index + pl) >= boundary + 2)) {
-                    matchedBoundary = boundary;
-                    break;
-                }
-            }
-            if (matchedBoundary != null) {
-                matchedIndex = index;
-                break;
-            }
-        }
-        if (matchedIndex != null) {
+	private CircProtein translate(final String sequence) {
+		if (sequence == null) {
+			throw new IllegalArgumentException("The DNA/RNA sequence can not be null");
+		}
+		String normalizedSequence = sequence.toUpperCase();
+		CircProtein protein = sequence2ProteinMapCache.get(normalizedSequence);
+		if (protein != null) {
+			return protein;
+		}
+		cacheLock.lock();
+		try {
+			if (protein == null) {
+				protein = toCirc(normalizedSequence, circLoop);
+				sequence2ProteinMapCache.put(sequence, protein);
+			}
+			return protein;
+		} finally {
+			cacheLock.unlock();
+		}
+	}
+
+	public static void main(String[] args) {
+		CircProteinMatcher matcher = new CircProteinMatcher();
+		final String rna = "ATTGCAGGAGGAGATGCTTCAGAGAGAGGAAGCCGAAAACACCCTGCAATCTTTCAGACAGGATGTGATTACAAGGATGACGACGATAAGTGACAATGCGTCTCTGGCACGTCTTGACCTTGAACGCAAAGTGGAATCTTTGCAAGAAGAGATTGCCTTTTTGAAGAAACTCCACGAAGAGGAAATCCAGGAGCTGCAGGCTCAGATTCAGGAACAGCATGTCCAAATCGATGTGGATGTTTCCAAGCCTGACCTCACGGCTGCCCTGCGTGACGTACGTCAGCAATATGAAAGTGTGGCTGCCAAGAACCTGCAGGAGGCAGAAGAATGGTACAAATCCAAGTTTGCTGACCTCTCTGAGGCTGCCAACCGGAACAATGACGCCCTGCGCCAGGCAAAGCAGGAGTCCACTGAGTACCGGAGACAGGTGCAGTCCCTCACCTGTGAAGTGGATGCCCTTAAAGGAACCAATGAGTCCCTGGAACGCCAGATGCGTGAAATGGAAGAGAACTTTGCCGTTGAAGCTGCTAACTACCAAGACACTATTGGCCGCCTGCAGGATGAGATTCAGAATATGAAGGAGGAAATGGCTCGTCACCTTCGTGAATACCAAGACCTGCTCAATGTTAAGATGGCCCTTGACATTGAGATTGCCACCTACAGGAAGCTGCTGGAAGGCGAGGAGAGCAGGATTTCTCTGCCTCTTCCAAACTTTTCCTCCCTGAACCTGAGGGAAACTAATCTGGATTCACTCCCTCTGGTTGATACCCACTCAAAAAGGACACTTCTGATTAAGACGGTTGAAACTAGAGATGGACAG";
+		CircProtein protein = matcher.translate(rna);
+		System.out.println(protein.getProtein());
+		System.out.println(Arrays.toString(protein.getBoundarys().toArray()));
+		matcher.match(null, rna, "VETRDGQIAGGDASER");
+	}
+
+	private MatchResult match(final String key, final String RNA, final String protein) {
+		// cache the RNA
+		CircProtein circProtein = translate(RNA);
+		String target = circProtein.getProtein();
+		List<Integer> boundarys = circProtein.getBoundarys();
+		// do match
+		final int pl = protein.length();
+		Integer matchedBoundary = null;
+		Integer matchedIndex = null;
+		for (int fromIndex = 0; fromIndex < target.length(); fromIndex++) {
+			int index = target.indexOf(protein, fromIndex);
+			if (index == -1) {
+				break;
+			}
+			fromIndex = index + 1;
+			// check boundary
+			if (checkBoundary) {
+				for (Integer boundary : boundarys) {
+					if ((index <= boundary - 2) && ((index + pl) >= boundary + 2)) {
+						matchedBoundary = boundary;
+						break;
+					}
+				}
+				if (matchedBoundary != null) {
+					matchedIndex = index;
+					break;
+				}
+			} else {
+				matchedIndex = index;
+				break;
+			}
+		}
+		if (matchedIndex != null) {
 //			System.out.print(String.format("%d, %d", matchedIndex, matchedBoundary));
-        }
-        MatchResult result = null;
-        if (matchedIndex != null && matchedBoundary != null) {
-            result = new MatchResult();
-            result.setDnaKey(RNAKey);
-            result.setDnaSequence(RNA);
-            result.setIndex(matchedIndex);
-            result.setBoundary(matchedBoundary);
-            result.setProtein(protein);
-        }
-        return result;
-    }
+		}
+		MatchResult result = null;
+		if (matchedIndex != null) {
+			result = new MatchResult();
+			result.setDnaKey(key);
+			result.setDnaSequence(RNA);
+			result.setIndex(matchedIndex);
+			result.setBoundary(matchedBoundary);
+			result.setProtein(protein);
+		}
+		return result;
+	}
 
-    @Override
-    public List<MatchResult> match(final RNAProvider provider, final String protein) {
-        if (provider == null || protein == null || protein.isBlank()) {
-            return null;
-        }
-        List<MatchResult> matchedSequences = new ArrayList<>();
-        ReentrantLock writeLock = new ReentrantLock();
-        provider.entrySet().parallelStream().forEach(entry -> {
-            String id = entry.getKey();
-            String sequence = entry.getValue();
-            if (sequence == null || sequence.isBlank()) {
-                return;
-            }
-            MatchResult result = match(id, sequence, protein.toUpperCase());
-            if (result != null) {
-                writeLock.lock();
-                try {
-                    matchedSequences.add(result);
-                } finally {
-                    writeLock.unlock();
-                }
-            }
-        });
-        return matchedSequences;
-    }
+	@Override
+	public List<MatchResult> match(final RNAProvider provider, final String protein) {
+		if (provider == null || protein == null || protein.isBlank()) {
+			return null;
+		}
+		List<MatchResult> matchedSequences = new ArrayList<>();
+		ReentrantLock writeLock = new ReentrantLock();
+		provider.entrySet().parallelStream().forEach(entry -> {
+			String id = entry.getKey();
+			String sequence = entry.getValue();
+			if (sequence == null || sequence.isBlank()) {
+				return;
+			}
+			MatchResult result = match(id, sequence, protein.toUpperCase());
+			if (result != null) {
+				writeLock.lock();
+				try {
+					matchedSequences.add(result);
+				} finally {
+					writeLock.unlock();
+				}
+			}
+		});
+		return matchedSequences;
+	}
 }
